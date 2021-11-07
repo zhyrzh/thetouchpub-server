53db9c5aad26b1fac86f9cd9082c59e9639e5041
rhyzzz <rhyzzz@rhyz-pop-pc.bbrouter>
2021-11-07 21:29:44 +0800

    specified node environment

--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,17 @@
 const { Pool } = require("pg");
-
-module.exports = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+const config =
+  process.env.NODE_ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {
+        database: "thetouchpub_db",
+        user: "postgres",
+        password: "sjcs2012AdminRhyz",
+        port: 5432,
+        host: "localhost",
+      };
+module.exports = new Pool(config);
