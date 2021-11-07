ee6b88a995ce2a3f73e34a1bb60b2fb255df941a
rhyzzz <rhyzzz@rhyz-pop-pc.bbrouter>
2021-11-07 11:17:28 +0800

    setup production database connection

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,9 @@
 const touchPubRouter = require("express").Router();
 const articleRoutes = require("./article");
 
+touchPubRouter.get("/", (req, res) => {
+  res.send("<h1>Production API</h1>");
+});
 touchPubRouter.use("/api/article", articleRoutes);
 
 module.exports = touchPubRouter;
