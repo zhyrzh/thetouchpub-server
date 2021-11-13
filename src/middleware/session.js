194375223640523ae55339c833abb929b9776288
rhyzzz <rhyzzz@rhyz-pop-pc.bbrouter>
2021-11-14 03:37:44 +0800

    load user fix

--- src/middleware/session.js
+++ src/middleware/session.js
@@ -14,6 +14,7 @@ module.exports = session({
   cookie: {
     maxAge: 1000 * 60 * 60 * 24 * 365,
     secure: process.env.NODE_ENV === "production",
+    sameSite: "none",
   },
   secret: session_secret,
   resave: false,
