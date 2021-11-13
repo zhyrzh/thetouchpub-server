fbaee17abdcc84a86f855f0889710341c5e034ee
rhyzzz <rhyzzz@rhyz-pop-pc.bbrouter>
2021-11-14 06:06:42 +0800

    set cookie fix

--- src/middleware/verify-user.js
+++ src/middleware/verify-user.js
@@ -1,9 +1,5 @@
 module.exports = (req, res, next) => {
-  console.log(req.session, "bump");
-  console.log(req.cookies, "bump cookie");
-  console.log(req.protocol, "bump protocol");
-  console.log(req.headers.cookie, "bump header.cookie");
-  if (!req.session.admin)
+  if (!req.session.thetouchpub)
     return res
       .status(401)
       .json({ success: false, message: "you are not authorized to do this" });
