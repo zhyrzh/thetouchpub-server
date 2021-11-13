4b5d8e9a70ca0430b8806128d34d39b40b14d0e6
rhyzzz <rhyzzz@rhyz-pop-pc.bbrouter>
2021-11-14 00:00:45 +0800

    cors fix

--- src/server.js
+++ src/server.js
@@ -9,7 +9,12 @@ const customParseFormat = require("dayjs/plugin/customParseFormat");
 const session = require("./middleware/session");
 dayjs.extend(customParseFormat);
 
-app.use(cors("*"));
+app.use(
+  cors({
+    origin: "&",
+    credentials: true,
+  })
+);
 app.use(session);
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50MB" }));
