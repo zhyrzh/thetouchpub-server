3a0562f50e94ce2f4acbd67ddbbff91fca7b047c
rhyzzz <rhyzzz@rhyz-pop-pc.bbrouter>
2021-11-14 05:55:16 +0800

    set cookie fix

--- src/middleware/verify-user.js
+++ src/middleware/verify-user.js
@@ -1,6 +1,8 @@
 module.exports = (req, res, next) => {
   console.log(req.session, "bump");
   console.log(req.cookies, "bump cookie");
+  console.log(req.protocol, "bump protocol");
+  console.log(req.headers.cookie, "bump header.cookie");
   if (!req.session.admin)
     return res
       .status(401)
